0b390f9d6fd6e80897fcbb56e03f1deba4b8d3d4
statquant <statquant@outlook.com>
2024-07-09 12:23:15 +0100

    improvements to R

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,13 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Pane navigation
-keymap.set("n", "<C-h>", "<C-w>h", opts)
+--keymap.set("n", "<C-h>", "<C-w>h", opts)
 keymap.set("n", "<C-left>", "<C-w>h", opts)
-keymap.set("n", "<C-j>", "<C-w>j", opts)
+--keymap.set("n", "<C-j>", "<C-w>j", opts)
 keymap.set("n", "<C-down>", "<C-w>j", opts)
-keymap.set("n", "<C-k>", "<C-w>k", opts)
+--keymap.set("n", "<C-k>", "<C-w>k", opts)
 keymap.set("n", "<C-up>", "<C-w>k", opts)
-keymap.set("n", "<C-l>", "<C-w>l", opts)
+--keymap.set("n", "<C-l>", "<C-w>l", opts)
 keymap.set("n", "<C-right>", "<C-w>l", opts)
 
 -- Move to previous/next
@@ -22,15 +22,15 @@ vim.api.nvim_set_keymap('n', '<C-S-Up>', '<Cmd>BufferMovePrevious<CR>', { norema
 vim.api.nvim_set_keymap('n', '<C-S-Down>', '<Cmd>BufferMoveNext<CR>', { noremap = true, silent = true })
 
 -- Pane navigation
-keymap.set("n", "<leader><left>", ":bp<CR>", opts)
-keymap.set("n", "<leader><right>", ":bn<CR>", opts)
-keymap.set("i", "<leader><left>", "<Esc>:bp<CR>i", opts)
-keymap.set("i", "<leader><right>", "<Esc>:bn<CR>i", opts)
+-- keymap.set("n", "<leader><left>", ":bp<CR>", opts)
+-- keymap.set("n", "<leader><right>", ":bn<CR>", opts)
+-- keymap.set("i", "<leader><left>", "<Esc>:bp<CR>i", opts)
+-- keymap.set("i", "<leader><right>", "<Esc>:bn<CR>i", opts)
 
 -- Split
-keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
-keymap.set("n", "<leader>sh", ":split<CR>", opts)
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts)
+--keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
+--keymap.set("n", "<leader>sh", ":split<CR>", opts)
+--keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts)
 
 -- Indenting
 keymap.set("v", "<", "<gv")
--- lua/plugins/r-nvim.lua
+++ lua/plugins/r-nvim.lua
@@ -1,28 +1,4 @@
 local config = function ()
-  -- Create a table with the options to be passed to setup()
-  -- Function to find the root directory of the R package
-  function find_package_root()
-    local dir = vim.fn.expand('%:p:h')
-    while dir ~= '/' do
-      if vim.fn.filereadable(dir .. '/DESCRIPTION') == 1 then
-        return dir
-      end
-      dir = vim.fn.fnamemodify(dir, ':h')
-    end
-    return nil
-  end
-
-  -- Function to run devtools::load_all with the correct path
-  function load_all()
-    local package_root = find_package_root()
-    if package_root then
-      local cmd = string.format('devtools::load_all("%s")', package_root)
-      vim.fn['SendCmdToR'](cmd)
-    else
-      print("DESCRIPTION file not found in any parent directory")
-    end
-  end
-  vim.api.nvim_buf_set_keymap(0, "n", "<leader>la", ":lua load_all()<CR>", {})
 
   -- Set up the keybinding
 
@@ -30,25 +6,67 @@ local config = function ()
     -- Options passed to R
     R_args = {"--quiet", "--no-save"},
     -- open terminal in another tmux pane
-    external_term = "tmux split-window -vf ",
+    external_term = "tmux split-window -v ",
     -- Use radian for terminal
     -- R_app = "radian",
     -- config_tmux = false,
+    -- Only user mappings
+    user_maps_only = true,
     hook = {
-      after_config = function ()
-        -- This function will be called at the FileType event
-        -- of files supported by R.nvim. This is an
-        -- opportunity to create mappings local to buffers.
+      on_filetype = function ()
         if vim.o.syntax ~= "rbrowser" then
-          vim.api.nvim_buf_set_keymap(0, "n", "<Leader><CR>", "<Plug>RDSendLine<Esc>i", {})
-          vim.api.nvim_buf_set_keymap(0, "i", "<Leader><CR>", "<Esc><Plug>RDSendLine<Esc>i", {})
+          -- Start/Stop
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>rf", "<Plug>RStart", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>rq", "<Plug>RClose", {})
+          -- Default sending
           vim.api.nvim_buf_set_keymap(0, "n", "<Space>", "<Plug>RDSendLine", {})
           vim.api.nvim_buf_set_keymap(0, "v", "<Space>", "<Plug>RSendSelection", {})
-          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>ro", "<Esc><Plug>RInsertLineOutput", {})
-          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>ro", "<Esc><Plug>RInsertLineOutput<Esc>i", {})
-          vim.api.nvim_buf_set_keymap(0, "v", "<LocalLeader>zl", "<Cmd>lua require('r.run').action('pkg_load::load_all(here::here())')<CR>", {})
-          vim.api.nvim_buf_set_keymap(0, "v", "<LocalLeader>zd", "<Cmd>lua require('r.run').action('pkg_load::document(here::here())')<CR>", {})
+          -- More movements
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>z<CR>", "<Plug>RDSendLine", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>z<CR>", "<Esc><Leader>z<CR>i", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>z<up>", "<Plug>RSendAboveLines", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>z<up>", "<Esc><Leader>z<up>i", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>z<left>", "<Plug>RNLeftPart", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>z<left>", "<Esc><Leader>z<left>i", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>z<right>", "<Plug>RNRightPart", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>z<right>", "<Esc><Leader>z<right>i", {})
+          -- Advanced
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>zh", "<Plug>RHelp", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>zh", "<Esc><Leader>zhi", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>zo", "<Plug>RInsertLineOutput", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>zo", "<Esc><Leader>zoi", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>zp", "<Plug>RObjectPr", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>zp", "<Esc><Leader>zvi", {})
+          vim.api.nvim_buf_set_keymap(0, "n", "<Leader>za", "<Plug>RShowArgs", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<Leader>za", "<Esc><Leader>zai", {})
+          -- That should work with the current buffer path
+          vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>dg", "<Cmd>lua require('r.run').action('debugonce')<CR>", {})
+          vim.api.nvim_buf_set_keymap(0, "i", "<LocalLeader>dg", "<Esc><Leader>dgi", {})
         end
+        -- Create a table with the options to be passed to setup()
+        -- Function to find the root directory of the R package
+        function find_package_root()
+          local dir = vim.fn.expand('%:p:h')
+          while dir ~= '/' do
+            if vim.fn.filereadable(dir .. '/DESCRIPTION') == 1 then
+              return dir
+            end
+            dir = vim.fn.fnamemodify(dir, ':h')
+          end
+          return nil
+        end
+
+        -- Function to run devtools::load_all with the correct path
+        function load_all()
+          local package_root = find_package_root()
+          if package_root then
+            local cmd = string.format('devtools::load_all("%s")', package_root)
+            vim.fn['SendCmdToR'](cmd)
+          else
+            print("DESCRIPTION file not found in any parent directory")
+          end
+        end
+        vim.api.nvim_buf_set_keymap(0, "n", "<leader>la", ":lua load_all()<CR>", {})
       end
     },
     min_editor_width = 72,
@@ -61,6 +79,15 @@ local config = function ()
       "RSPlot",
       "RSaveClose",
       "ROBToggle",
+      "RFormatSubsetting", --Replace all the `$` subsetting operators with `[[` in the current buffer.
+      "RFormatNumbers", -- Add an 'L' suffix after numbers to explicitly indicate them as integers.
+      "RSeparatePathHere", --Split the path of the file under the cursor and open it using the here() prefix function
+      "RSeparatePathPaste",
+      "RSetwd", --Send to R setwd(<directory of current document>)
+      "RShowRout", --R CMD BATCH the current document and show the output in a new tab
+      "RSendMotion",
+      "ROBCloseLists",
+      "ROBOpenLists"
     },
   }
   -- Check if the environment variable "R_AUTO_START" exists.
