4587935f6452f4865306707095450c7c3f516bcf
statquant <statquant@outlook.com>
2024-07-09 15:56:35 +0100

    improvements to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,28 +2,47 @@ local keymap = vim.keymap
 
 local config = function()
   local telescope = require('telescope')
+  local work_dir = nil
+  if vim.fn.expand("$USER") == "statquant" then 
+    work_dir = {
+      string.format("%s/Work/", vim.fn.expand("$HOME")),
+    }
+  else
+    work_dir = {
+      string.format("/sqpc/hpc/sp_auction_np/%s/public/", vim.fn.expand("$USER")),
+      string.format("/sqpc/hpc/sp_auction_np/%s/private/", vim.fn.expand("$USER")),
+      string.format("/sqpc/scratch/mid_freq/%s/private/", vim.fn.expand("$USER")),
+      string.format("/sqpc/scratch/mid_freq/%s/public/", vim.fn.expand("$USER")),
+    }
+  end 
+  local search_dirs = vim.list_extend({vim.fn.expand('%:p:h')}, work_dir)
+
   telescope.setup({
     defaults = {
       mappings = {
         i = {
-          ["<C-j>"] = "move_selection_next",
-          ["<C-k>"] = "move_selection_previous"
+          -- Arrow keys are working fine
+          -- ["<C-j>"] = "move_selection_next",
+          -- ["<C-k>"] = "move_selection_previous"
         },
       },
     },
     pickers = {
       find_files = {
+        search_dirs = search_dirs,
         theme = "dropdown",
-        previewr = false,
+        previewr = true,
         hidden = true
       },
       live_grep = {
+        search_dirs = search_dirs,
         theme = "dropdown",
-        previewer = false,
+        previewer = true,
       },
       find_buffer = {
+        search_dirs = search_dirs,
         theme = "dropdown",
-        previewer = false,
+        previewer = true,
       }
     },
   })
@@ -36,9 +55,13 @@ return {
   config = config,
   keys = {
     keymap.set('n', '<leader>f', ":Telescope find_files<CR>"),
+    keymap.set('i', '<leader>f', "<Esc><leader>f"),
     keymap.set('n', '<leader>g', ":Telescope live_grep<CR>"),
+    keymap.set('i', '<leader>g', "<Esc><leader>g"),
     keymap.set('n', '<leader>b', ":Telescope buffers<CR>"),
+    keymap.set('i', '<leader>b', "<Esc><leader>b"),
     keymap.set('n', '<leader>k', ":Telescope keymaps<CR>"),
+    keymap.set('i', '<leader>k', "<Esc><leader>k"),
   },
   enabled = true,
 }
